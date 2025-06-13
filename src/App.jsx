cb7eee8ad62b21b195d541bdcd8f5ccfdf0427df
pritika <kadampritika0511@gmail.com>
2025-06-13 09:16:56 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import Header from './Components/header/Header';
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+import ContactPage from "./Pages/ContactPage";
+import CoursesPage from "./Pages/CoursesPage";
+import AdmissionsPage from "./Pages/AdmissionPage";
+import Footer from "./Components/footer/Footer";
+import "./styles/Home.css";
+import "./styles/About.css";
+import "./styles/Contact.css";
+import "./styles/Admission.css";
+import "./styles/Courses.css";
+import ChatbotComponent from "./Components/Chatbot/ChatbotComponents";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App () {
+    return (
+        <Router>
+            <div className="main-layout">
+                <Header/>
+                 <div className="content">
+                    <Routes>
+                        <Route path="/" element={< HomePage/>}/>
+                         <Route path="/contact" element={< ContactPage/>}/>
+                         <Route path="/about" element={< AboutPage/>}/>
+                         <Route path="/courses" element={< CoursesPage/>}/>
+                         <Route path="/admission" element={< AdmissionsPage/>}/>              
+                    </Routes>
+                </div>
+                <ChatbotComponent/>
+                <Footer/>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+
+export default App;
